c2f8132f9c5c81417bec1528614d1ebc6f26aa8a
Caroline Fassot <fassot.caroline@gmail.com>
2021-09-10 21:28:40 +0200

    creation calendar page

---
+++ lib/calendar_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CalendarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+           
+          ],
+        ),
+      ),
+    );
+  }
+}
